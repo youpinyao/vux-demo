cafe1754f3eac6d06a6e902e5ab748d09c7ef2aa
游品尧 <497400448@qq.com>
2017-10-31 16:42:09 +0800

    1

--- build/vux-config.js
+++ build/vux-config.js
@@ -21,21 +21,8 @@ module.exports = {
       envs: ['development']
     },
     {
-      name: 'i18n',
-      vuxStaticReplace: false,
-      staticReplace: false,
-      extractToFiles: 'src/locales/components.yml',
-      localeList: ['en', 'zh-CN']
-    },
-    {
       name: 'less-theme',
       path: 'src/theme.less'
     }
   ]
-}
-
-function toDash(str) {
-  return str.replace(/([A-Z])/g, function(m, w) {
-    return '-' + w.toLowerCase();
-  }).replace('-', '')
 }
